b40b1199cb8b78a4bafddac6561cdab803fd5e26
Pathum Bandara <pathumbandarame@gmail.com>
2023-12-28 18:52:08 +0530

    base

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {NotFoundPageComponent} from "./components/not-found-page/not-found-page.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo:'starter/home', pathMatch:'full'},
+  { path: 'security', loadChildren: () => import('./modules/security/security.module').then(m => m.SecurityModule) },
+  { path: 'shared', loadChildren: () => import('./modules/shared/shared.module').then(m => m.SharedModule) },
+  { path: 'starter', loadChildren: () => import('./modules/starter/starter.module').then(m => m.StarterModule) },
+  { path: '**', component:NotFoundPageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
